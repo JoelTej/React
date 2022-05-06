bb8bb1b3e6b43cfac2edbfafbe3c715c506f378e
Joel Tejada <joel.tejada@estudiantat.upc.edu>
2022-05-06 10:31:27 +0200

    Add files via upload

---
+++ redux-app/tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+    },
+    colors: {
+        "special-gray": '#F5F0F0',
+        red: colors.red,
+        white: colors.white,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
